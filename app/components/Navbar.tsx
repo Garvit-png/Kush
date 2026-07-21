9bb86e0e6c4482d8ce0119c0931a4849ecf26aa4
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-07-21 17:26:54 +0530

    Add profile photo in navbar avatar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function Navbar() {
   return (
     <header className="header">
       <div className="header__profile">
         <div className="avatar-ring">
-          <div className="avatar-fill"></div>
+          <Image
+            src="/photo.jpg"
+            alt="Kush Adhana"
+            width={44}
+            height={44}
+            className="avatar-photo"
+            priority
+          />
         </div>
         <h3 className="header__name">KUSH ADHANA</h3>
       </div>
